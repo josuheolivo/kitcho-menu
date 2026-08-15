b330e70fab96170eb42f4ce43a329875e93753db
arcanacloud <arcanacould@gmail.com>
2026-08-15 02:28:13 +0200

    fix(lint): remove unused import in A5QrPosterModal

--- src/components/A5QrPosterModal.tsx
+++ src/components/A5QrPosterModal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRef, useState } from 'react';
-import { SparkIcon, CheckIcon } from './Icons';
+import { SparkIcon } from './Icons';
 
 interface A5QrPosterModalProps {
   isOpen: boolean;
